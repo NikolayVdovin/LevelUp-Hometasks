e319a7f848e91dae4af38a7a826d77a903e47864
NikolayVdovin <nikolay-vdovin@list.ru>
2023-02-07 21:46:29 +0300

    Add repository

---
+++ Lesson2.Resume/Resume.cs
@@ -0,0 +1,49 @@
+﻿namespace Lesson2.Resume
+{
+    internal class Resume
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Hobby { get; set; }
+
+        public Resume(string name, string surname, int age, string hobby)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Hobby = hobby;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name} \nSurname: {Surname} \nAge: {Age}\nHobby: {Hobby}";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name:");
+            string nameFromConsole = Console.ReadLine();
+
+            Console.WriteLine("Enter surname:");
+            string surnameFromConsole = Console.ReadLine();
+
+            Console.WriteLine("Enter age:");
+            int ageFromConsole;
+
+
+            while (!int.TryParse(Console.ReadLine(), out ageFromConsole))
+            {
+                Console.WriteLine("Please, enter the valid age. It should be a number");
+            }
+
+            Console.WriteLine("Enter hobby:");
+            string hobbyFromConsole = Console.ReadLine();
+
+            Resume person = new Resume(nameFromConsole, surnameFromConsole, ageFromConsole, hobbyFromConsole);
+
+            Console.WriteLine(person);
+
+
+        }
+    }
+}
---
+++ Lesson2.Store/Store.cs
@@ -0,0 +1,28 @@
+﻿namespace Lesson2.Store
+{
+    internal class Store
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public Store(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Store name is \"{Name}\" and store address is \"{Address}\"");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter store name");
+            string nameStore = Console.ReadLine();
+
+            Console.WriteLine("Enter sore address");
+            string addressStore = Console.ReadLine();
+
+            Store store = new Store(nameStore, addressStore);
+            store.PrintInfo();
+        }
+    }
+}
