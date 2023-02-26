5f541d18c5fe1b09dba75f6ad0a9b46fe64aee06
NikolayVdovin <nikolay-vdovin@list.ru>
2023-02-26 17:31:03 +0300

    Добавлено решение

---
+++ Lesson4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lesson4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cache = new SimpleGenericCache<string>();
+            var value = "Hello!";
+
+            cache.Store("first", value, 0);
+            cache.Store("second", value);
+
+            Console.WriteLine(cache.Fetch("first"));
+            Console.WriteLine(cache.Fetch("second")); 
+        }
+    }
+}
---
+++ Lesson4/SimpleGenericCache.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lesson4
+{
+    internal class SimpleGenericCache <T>
+    {
+        private readonly Dictionary<string, CachedValue<T>> _cache = new();
+        internal void Store(string key, T value, int timeout = 30)
+        {
+            var CachedValue = new CachedValue<T>
+            {
+                CreationTime= DateTime.Now,
+                Value = value,  
+                Timeout = timeout   
+
+            };
+
+            _cache[key] = CachedValue;
+        }
+
+        internal CachedValue<T> Fetch(string key)
+        {
+            if (_cache.TryGetValue(key, out var value))
+            {
+                var timeHasPassed = value.CreationTime + TimeSpan.FromSeconds(value.Timeout);
+                if (timeHasPassed >= DateTime.Now)
+                { 
+                    return value;
+                }
+                else
+                {
+                    _cache.Remove(key);
+                }                          
+            }
+            return default;
+        }
+    }
+}
