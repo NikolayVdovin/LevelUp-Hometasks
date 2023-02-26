b050e7a0bb78be4244bfc62da77ede4b9c08ad3f
NikolayVdovin <nikolay-vdovin@list.ru>
2023-02-26 18:42:51 +0300

    Добавлено решение для 5 задачи

---
+++ Lesson5/ExtensionClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Lesson5
+{
+    public static class ExtensionClass
+    {
+        public static String ClearingLine(this string str)
+        {
+            var outString = Regex.Replace(str, @"[^0-9a-zA-Z:, ]+", "");
+            return outString;
+        }
+    }
+}
